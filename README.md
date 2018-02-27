f58e927d58030f214ed17be434b28e6723c95d96
alex <alex.tran234@gmail.com>
2018-02-27 22:08:13 +1100

    clone from l2

---
+++ README.md
@@ -0,0 +1,9 @@
+# CryptoZombies, lesson 2
+
+I followed the tutorial from https://share.cryptozombies.io/en/lesson/2/share/alext?id=Z2l0aHVifDE2MjQ1MjI=
+ 
+
+In addition, unit tests have been added.
+
+To build and test, `dapp` is required, similar to this repository https://github.com/alext234/dapp-tutorial.
+
---
+++ src/ZombieFactory.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.19;
+
+contract ZombieFactoryEvents {
+    event NewZombie(uint zombieId, string name, uint dna);    
+}
+
+contract ZombieFactory is ZombieFactoryEvents {
+
+	uint dnaDigits = 16;
+	uint dnaModulus = 10 ** dnaDigits;
+
+	struct Zombie {
+		string name;
+		uint dna;
+	}
+
+	Zombie[] public zombies;
+
+	mapping (uint => address) public zombieToOwner;
+	mapping (address => uint) ownerZombieCount;
+
+	function _createZombie(string _name, uint _dna) internal {
+		uint id = zombies.push(Zombie(_name, _dna)) - 1;
+		zombieToOwner[id] = msg.sender;
+		ownerZombieCount[msg.sender]++;
+		NewZombie(id, _name, _dna);
+	}
+
+	function _generateRandomDna(string _str) private view returns (uint) {
+		uint rand = uint(keccak256(_str));
+		return rand % dnaModulus;
+	}
+
+	function createRandomZombie(string _name) public {
+		require(ownerZombieCount[msg.sender] == 0);
+		uint randDna = _generateRandomDna(_name);
+		randDna = randDna - randDna % 100;
+		_createZombie(_name, randDna);
+	}
+
+	function getZombiesCount() public view returns (uint) {
+		return zombies.length;
+	}
+	
+	function getDnaByIndex(uint idx) public view returns (uint) {
+		return zombies[idx].dna;    
+	}
+}
